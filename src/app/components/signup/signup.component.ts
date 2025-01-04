ac4e05c957952f787eee652363879390d97b4aef
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-01-04 22:13:15 +0100

    add signup component with form, password visibility toggle, and styles; update start-site and navigation components

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  password!: string;
+
+  showPassword = false;
+  showConfirmPassword = false;
+
+
+  ngOnInit() {
+    this.password = 'password';
+  }
+
+  togglePasswordVisibility(field: string) {
+
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else if (field === 'confirmPassword') {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
 }
--- src/app/components/start-site/start-site.component.ts
+++ src/app/components/start-site/start-site.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-start-site',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './start-site.component.html',
   styleUrl: './start-site.component.scss'
 })
