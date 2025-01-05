9b27c948849858436ad33b52c4cd64c7bf1e7259
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-01-05 09:58:13 +0100

    add login and signup components with form validation, password visibility toggle, and styles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  password!: string;
+  showPassword = false;
+  error = false;
+
+  ngOnInit() {
+    this.password = 'password';
+  }
+
+  togglePasswordVisibility(field: string) {
+
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    }
+  }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -15,6 +15,7 @@ export class SignupComponent {
 
   showPassword = false;
   showConfirmPassword = false;
+  error = false;
 
 
   ngOnInit() {
