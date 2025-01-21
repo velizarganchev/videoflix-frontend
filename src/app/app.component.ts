f3f06f4fe1f8bc4274b3f8da2036bfbd2ff5ee5d
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-01-21 17:31:43 +0100

    add NotFoundComponent and FooterComponent; restructure app layout and remove deprecated home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ErrorService } from './services/error.service';
 import { ErrorToastComponent } from "./shared/error-toast/error-toast.component";
+import { NavigationComponent } from "./shared/navigation/navigation.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ErrorToastComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ErrorToastComponent,
+    NavigationComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { StartSiteComponent } from './components/start-site/start-site.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -14,33 +14,31 @@ export const routes: Routes = [
         redirectTo: 'start-site'
     },
     {
-        path: '',
-        component: HomeComponent,
-        children: [
-            {
-                path: 'start-site',
-                component: StartSiteComponent
-            },
-            {
-                path: 'signup',
-                component: SignupComponent
-            },
-            {
-                path: 'login',
-                component: LoginComponent
-            },
-            {
-                path: 'forgot-password',
-                component: ForgotPasswordComponent
-            },
-            {
-                path: 'reset-password',
-                component: ResetPasswordComponent
-            },
-            {
-                path: 'main-content',
-                component: MainContentComponent
-            }
-        ]
+        path: 'start-site',
+        component: StartSiteComponent
+    },
+    {
+        path: 'main-content',
+        component: MainContentComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent
+    },
+    {
+        path: 'reset-password',
+        component: ResetPasswordComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
