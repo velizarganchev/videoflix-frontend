0a21f4cda1eb2c806eda354765aad1626d95f02e
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-01-21 20:09:53 +0100

    add authGuard to routes for protected access to main content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, RedirectCommand, CanMatchFn, Router } from '@angular/router';
 import { StartSiteComponent } from './components/start-site/start-site.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
@@ -6,6 +6,18 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { inject } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
+const authGuard: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const authService = inject(AuthService);
+    const isAuthenticated = authService.getUser();
+    if (isAuthenticated) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/start-site'));
+};
 
 export const routes: Routes = [
     {
@@ -19,7 +31,8 @@ export const routes: Routes = [
     },
     {
         path: 'main-content',
-        component: MainContentComponent
+        component: MainContentComponent,
+        canMatch: [authGuard]
     },
     {
         path: 'signup',
